6aa788f813b8f6ec8f1c58e099987a06c2251f73
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-18 20:12:21 +0200

    [UPD] added navbar in show.blade

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,13 +1,15 @@
-@extends('layouts.admin')
+@extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <p>{{ $project->description }}</p>
-    <p><strong>Linguaggi:</strong> {{ $project->languages }}</p>
-    <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-warning">Modifica</a>
-    <form action="{{ route('projects.destroy', $project->id) }}" method="POST" style="display:inline-block;">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">Delete</button>
-    </form>
+    <div class="container">
+        <h1>{{ $project->title }}</h1>
+        <p>{{ $project->description }}</p>
+        <p><strong>Linguaggi:</strong> {{ $project->languages }}</p>
+        <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-warning">Modifica</a>
+        <form action="{{ route('projects.destroy', $project->id) }}" method="POST" style="display:inline-block;">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+    </div>
 @endsection
